9556c96ad88fd8567d3f96ccdf2353d25927d893
Edem Agbenyo <gedem4r@gmail.com>
2020-04-13 13:03:53 +0000

    lifting board to app

--- src/App.js
+++ src/App.js
@@ -3,5 +3,14 @@ import "./styles.css";
 import Board from "./Board";
 
 export default function App() {
-  return <Board />;
+  // const [state, setState] = useState({
+  //   history:[{squares: Array(9).fill(null)}],
+  //   xnext: true
+  // });
+  console.log("boom ")
+  return (
+    <div>
+      <Board />
+    </div>
+  );
 }
